44af05a672e786741655eb394cb88e05cea580f1
kjweng <wengkj@gmail.com>
2015-07-18 14:49:23 -0400

    Test

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var http = require("http");
 var express = require("express");
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function (req, res) {
   res.send('Hello World! Hello malcom');
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(app.get("port"), function () {
   var host = server.address().address;
   var port = server.address().port;
 
